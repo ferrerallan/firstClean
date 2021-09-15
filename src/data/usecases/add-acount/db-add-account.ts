f44b00880905e50df4bf7a991917d20c8b38c5d2
Allan <ferrerallan@gmail.com>
2021-09-15 18:37:32 -0300

    chore: adicionando bibliotecas do jestmongo

--- src/data/usecases/add-acount/db-add-account.ts
+++ src/data/usecases/add-acount/db-add-account.ts
@@ -1,17 +1,30 @@
+/* eslint-disable arrow-parens */
 import { AccountModel } from '../../../domain/models/account';
-import { IAddAccount, IAddAccountModel } from '../../../domain/usecases/addAccount';
+import {
+  IAddAccount,
+  IAddAccountModel,
+} from '../../../domain/usecases/addAccount';
 import { IAddAccountRepository } from '../../protocols/add-account-repository';
 import { IEncrypter } from '../../protocols/encrypter';
 
 export class DbAddAccount implements IAddAccount {
   private readonly encrypter: IEncrypter;
 
-  constructor(encrypter: IEncrypter, addAccountRepository: IAddAccountRepository) {
+  private readonly addAccountRepository: IAddAccountRepository;
+
+  constructor(
+    encrypter: IEncrypter,
+    addAccountRepository: IAddAccountRepository,
+  ) {
     this.encrypter = encrypter;
+    this.addAccountRepository = addAccountRepository;
   }
 
-  async add(account: IAddAccountModel): Promise<AccountModel> {
-    this.encrypter.encrypt(account.password);
-    return new Promise((resolve) => resolve(null));
+  async add(accountData: IAddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password);
+    const newAccount = this.addAccountRepository.add(
+      Object.assign({}, accountData, { password: hashedPassword }),
+    );
+    return new Promise(resolve => resolve(newAccount));
   }
 }
